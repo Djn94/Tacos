59f72e7e533495028e5718eb9780c90d3b696fc4
Non Dewman <Dnewman1934@yahoo.com>
2019-05-26 15:15:15 -0500

    all set up

---
+++ controllers/tacos_controller.js
@@ -0,0 +1,5 @@
+require("../models/taco")(app);
+const express = require("express");
+// selectAll()` __ MYSQL query to select * from tavos
+//      * `insertOne()`   MYSQL query to add tacos
+//      * `updateOne()`  MYSQL query to change attibute of tacos
---
+++ models/taco.js
@@ -0,0 +1,5 @@
+module.export(function (app) {
+    require("../config/orm")(app);
+
+    //idk what this means yet
+});
