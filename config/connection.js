04b997aca3f2e6e5cb75ddc6aaef5a1af20dafca
Non Dewman <Dnewman1934@yahoo.com>
2019-05-26 15:04:02 -0500

    working mysql connection

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "tacos_db"
+});
+
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    connection.query("SELECT * FROM tacos", function (err, res) {
+        if (err) throw err;
+        let tacoList = res;
+        console.log(tacoList);
+    }
+    )
+    connection.end();
+});
+
