d471abbcfeb390aa1e875938618130aa2e4b12b4
Non Dewman <Dnewman1934@yahoo.com>
2019-06-02 16:23:27 -0500

    orm looks like the activities, we'll see if it works oo

--- config/orm.js
+++ config/orm.js
@@ -1,27 +1,72 @@
 const connection = require("./connection.js");
+function printQuestionMarks(num) {
+    const arr = [];
+    for (const i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+};
+function objToSql(object) {
+    const arr = [];
+    for (var key in object) {
+        const value = object[key];
+        if (Object.hasOwnProperty.call(object, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+    return arr.toString();
+};
 const orm = {
-    selectAll: function (tableName) {
-        const queryString = "SELECT * FROM ??";
-        connection.query(queryString, [tableName], function (err, result) {
+    selectAll: function (tableName, callback) {
+        const queryString = "SELECT * FROM " + tableName + ";";
+        connection.query(queryString, function (err, result) {
             if (err) throw err;
-            console.log(result)
+            callback(result)
         });
     },
-    insertOne: function (taconame, shell, vegetarian, pickedup) {
-        const queryString = "INSERT INTO tacos SET ?";
-        //taco_name, shell, vegetarian, picked_up
-        connection.query(queryString, [taconame, shell, vegetarian, pickedup], function (err, result) {
+    insertOne: function (tableName, cols, vals, callback) {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+        console.log(queryString)
+        connection.query(queryString, vals, function (err, result) {
             if (err) throw err;
-            console.log(result)
+
+            callback(result);
         })
     },
-    updateOne: function () {
-        const queryString = "";
-        connection.query(queryString, [], function (err, result) {
+    updateOne: function (tableName, objColVals, condition, callback) {
+        const queryString = "UPDATE "; +tableName;
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        connection.query(queryString, function (err, result) {
             if (err) throw err;
-            console.log(result)
+            callback(result);
         })
     },
-}
+    delete: function (tableName, condition, callback) {
+        const queryString = "DELETE FROM " + tableName;
+        queryString += " WHERE ";
+        queryString += condition;
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            callback(result);
+        });
+    }
+};
 module.exports = orm;
 
--- server.js
+++ server.js
@@ -1,15 +1,45 @@
-const express = require('express');
-const PORT = process.env.port || 8080;
-const app = express();
-const connection = require("./config/connection.js")
+// const express = require('express');
+// const PORT = process.env.port || 8080;
+// const app = express();
+// //const connection = require("./config/connection.js")
+// app.use(express.static("public"));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// const exphbs = require("express-handlebars");
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+// //const routes = require("./controllers/tacos_controller.js");
+// //app.use(routes);
+// app.listen(PORT, function () {
+//     console.log(`server listening on ${PORT}`)
+// })
+
+var express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
+
+// Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-const exphbs = require("express-handlebars");
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-//const routes = require("./controllers/tacos_controller.js");
-//app.use(routes);
+
+// Import routes and give the server access to them.
+// var routes = require("./controllers/catsController.js");
+
+// app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function () {
-    console.log(`server listening on ${PORT}`)
-})
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
