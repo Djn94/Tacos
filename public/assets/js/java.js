dc9928f86f08a93589dc6f665fe37ed5c9ac2e92
Non Dewman <Dnewman1934@yahoo.com>
2019-06-03 22:06:15 -0500

    java wont link correctly?

---
+++ public/assets/js/java.js
@@ -0,0 +1,18 @@
+$(function () {
+    console.log('java running')
+    $("#submitButton").on("submit", function (event) {
+        event.preventDefault();
+        console.log("it worked");
+    })
+
+
+
+
+
+
+
+
+
+
+
+});
