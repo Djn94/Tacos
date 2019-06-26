ef1eae4dedd4f6ac8539aa4d36e9a1dd4c00a2a4
Non Dewman <Dnewman1934@yahoo.com>
2019-06-26 14:41:38 -0500

    update

--- config/orm.js
+++ config/orm.js
@@ -21,7 +21,7 @@ function objToSql(object) {
 };
 const orm = {
     selectAll: function (tableName, callback) {
-        var queryString = "SELECT * FROM " + tablename + ";";
+        var queryString = "SELECT * FROM " + tableName + ";";
         connection.query(queryString, function (err, result) {
             if (err) throw err;
             callback(result)
