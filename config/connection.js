2e9b0e89560a4bdb5374059d85fcc92068689836
Non Dewman <Dnewman1934@yahoo.com>
2019-06-26 14:14:21 -0500

    config fix

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,16 @@
 const mysql = require("mysql");
-let connection;
+var connection;
 if (process.env.JAWSDB_URL) {
     console.log('The process jawas thing is runnin')
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "yhrz9vns005e0734.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+
+        host: "localhost",
         port: 3306,
-        user: "gvk3i44a2gxcmok3",
-        password: "y8hvph8b53ctysit",
-        database: "xh19lynmujp1jmsa"
-        // host: "localhost",
-        // port: 3306,
-        // user: "root",
-        // password: "root",
-        // database: "tacos_db"
+        user: "root",
+        password: "root",
+        database: "tacos_db"
     });
 }
 connection.connect(function (err) {
