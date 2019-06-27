c021307be9ccd20e6e41d70f0b9065b7d87f4acf
Non Dewman <Dnewman1934@yahoo.com>
2019-06-27 15:23:28 -0500

    database fix

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,11 @@
 USE tacos_db;
 INSERT INTO tacos (taco_name, shell, vegetarian, picked_up)
-VALUES ("carne asada", "hard", false, false);
+VALUES ("de Carne Asada", "hard", false, false);
 INSERT INTO tacos (taco_name, shell, vegetarian, picked_up)
-VALUES ("tofu", "soft", true, false);
+VALUES ("de Tofu", "soft", true, false);
 INSERT INTO tacos (taco_name, shell, vegetarian, picked_up)
 VALUES ("de Lengua", "hard", false, false);
 INSERT INTO tacos (taco_name, shell, vegetarian, picked_up)
-VALUES ("de gato", "hard", false, true);
+VALUES ("de Gato", "hard", false, true);
 
 SELECT * FROM tacos;
